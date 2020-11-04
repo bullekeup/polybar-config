f6172758d28eb426d76faa7814861f21fe628c78
Axel Moinet <axel.moinet@codium.fr>
2020-11-04 01:14:30 +0100

    Moved sleep 1 to after sending kill signal to existing polybar instances to prevent a bug where the script enters an infinite loop when restarting dwm

--- launch.sh
+++ launch.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 ## Add this to your wm startup file.
-[ -n "$(command -v displayset)" ] && { displayset; sleep 1; }
+[ -n "$(command -v displayset)" ] && displayset
 
 # Terminate already running bar instances
 killall -q polybar
+sleep 1;
 
 ## Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
